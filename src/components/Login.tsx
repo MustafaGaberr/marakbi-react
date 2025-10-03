0aa1cf358b4e02b12cce66450b8f257434db70cc
Mustafa Gaber <DrMustafaGaber@gmail.com>
2025-10-03 19:13:17 +0300

    setup

---
+++ src/components/Login.tsx
@@ -0,0 +1,45 @@
+function Login() {
+
+    return (
+      <>
+        
+
+<div style={{ width: '100%', height: '100%', position: 'relative', background: 'white', overflow: 'hidden' }}>
+    <img style={{ width: 618, height: 1024, left: 0, top: 0, position: 'absolute' }} src="https://placehold.co/618x1024" />
+    <div style={{ left: 868, top: 235, position: 'absolute', color: 'black', fontSize: 42, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word' }}>Welcome Back</div>
+    <div style={{ left: 816, top: 298, position: 'absolute', color: '#7D7D7D', fontSize: 22, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>log in to continue your adventure</div>
+    <div style={{ width: 539, left: 742, top: 378, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex' }}>
+        <div style={{ alignSelf: 'stretch', color: '#616161', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', textTransform: 'capitalize', wordWrap: 'break-word' }}>Email </div>
+        <div style={{ alignSelf: 'stretch', height: 60, padding: 12, background: '#F7F7F7', borderRadius: 8, justifyContent: 'flex-start', alignItems: 'center', gap: 66, display: 'inline-flex' }}>
+            <div style={{ color: '#7D7D7D', fontSize: 14, fontFamily: 'Poppins', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>Email address</div>
+        </div>
+    </div>
+    <div style={{ width: 539, left: 742, top: 493, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex' }}>
+        <div style={{ alignSelf: 'stretch', color: '#616161', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', textTransform: 'capitalize', wordWrap: 'break-word' }}>Password</div>
+        <div style={{ alignSelf: 'stretch', height: 60, padding: 12, background: '#F7F7F7', borderRadius: 8, justifyContent: 'flex-start', alignItems: 'center', gap: 66, display: 'inline-flex' }}>
+            <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#7D7D7D', fontSize: 14, fontFamily: 'Poppins', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>**************</div>
+        </div>
+    </div>
+    <div style={{ width: 326, height: 52, paddingLeft: 24, paddingRight: 24, paddingTop: 11, paddingBottom: 11, left: 849, top: 688, position: 'absolute', background: '#093B77', borderRadius: 8, justifyContent: 'center', alignItems: 'center', gap: 11, display: 'inline-flex' }}>
+        <div style={{ color: 'var(--Colors-Default-Static-static-100, white)', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word' }}>Log in </div>
+    </div>
+    <div style={{ left: 742, top: 600, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 9, display: 'inline-flex' }}>
+        <div style={{ width: 20, height: 20, borderRadius: 9999, border: '1px #7D7D7D solid' }} />
+        <div style={{ color: '#7D7D7D', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>Remember me</div>
+    </div>
+    <div style={{ left: 867, top: 764, position: 'absolute' }}><span style={{ color: '#7D7D7D', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>you don't have an account? </span><span style={{ color: '#106BD8', fontSize: 16, fontFamily: 'Poppins', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>Sign up</span></div>
+    <div style={{ left: 1140, top: 600, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 9, display: 'inline-flex' }}>
+        <div style={{ color: '#106BD8', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>Forget Password?</div>
+    </div>
+    <div style={{ width: 174, height: 174, left: 37, top: 27, position: 'absolute', background: 'rgba(255, 252.64, 252.64, 0.75)', boxShadow: '12.116991996765137px 12.116991996765137px 12.116991996765137px ', borderRadius: 9999, filter: 'blur(6.06px)' }} />
+    <div style={{ width: 58.73, height: 98.51, left: 98, top: 54, position: 'absolute' }}>
+        <div style={{ width: 12.08, height: 19.84, left: 5.86, top: 51.15, position: 'absolute', background: 'linear-gradient(180deg, #106BD8 0%, #083872 82%)' }} />
+        <div style={{ width: 27.41, height: 93.36, left: 5.86, top: -4.61, position: 'absolute', background: 'linear-gradient(180deg, #106BD8 0%, #083872 82%)' }} />
+        <div style={{ left: -7.69, top: 93.04, position: 'absolute', color: '#106BD8', fontSize: 17.53, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word' }}>Marakbi</div>
+    </div>
+</div>
+      </>
+    )
+  }
+  
+  export default Login
